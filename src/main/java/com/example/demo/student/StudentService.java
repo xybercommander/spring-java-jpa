ff22628728e73a3ddda112490129c283cc3d8e79
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-10-17 09:01:55 +0530

    initial commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,22 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Samrat Mukherjee",
+                        "samratmukherjee.sps@gmail.com",
+                        LocalDate.of(2001, Month.APRIL, 10),
+                        20
+                )
+        );
+    }
+}
